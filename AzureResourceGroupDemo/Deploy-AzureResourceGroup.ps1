b3a1fae10f7c67757f68d304e138b9e92ca21cd3
abhaydiwan <abhaydiwan@gmail.com>
2019-11-27 21:58:02 -0500

    Update Deploy-AzureResourceGroup.ps1

--- AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
@@ -1,3 +1,3 @@
-Set-AzureRmCurrentStorageAccount -ResourceGroupName armdemo -Name armdemo
+Set-AzureRmCurrentStorageAccount -ResourceGroupName armdemo -Name armdemonestedacc
 $url = (Get-AzureStorageBlob -Container test -Blob Demo1ParentTemplate.json).ICloudBlob.uri.AbsoluteUri
 New-AzureRmResourceGroupDeployment -ResourceGroupName armdemo -TemplateUri $url 
