7e4cc69e074c92cd928267bf50e5b2dbe9f6cabf
abhaydiwan <abhaydiwan@gmail.com>
2019-11-27 21:52:07 -0500

    Update Deploy-AzureResourceGroup.ps1

--- AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
@@ -1,4 +1,4 @@
 Set-AzureRmCurrentStorageAccount -ResourceGroupName armdemo -Name armdemonestedacc
 $token = New-AzureStorageContainerSASToken -Name token1 -Permission r -ExpiryTime (Get-Date).AddMinutes(30.0)
 $url = (Get-AzureStorageBlob -Container test -Blob Demo1ParentTemplate.json).ICloudBlob.uri.AbsoluteUri
-New-AzureRmResourceGroupDeployment -ResourceGroupName azurepipelineResGroup -TemplateUri ($url + $token) -containerSasToken $token
+New-AzureRmResourceGroupDeployment -ResourceGroupName azurepipelineResGroup -TemplateUri $url 
